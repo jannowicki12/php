009e9372fd950b7aa58a26a38226661e7fff4839
jannowicki12 <95223624+jannowicki12@users.noreply.github.com>
2022-11-28 14:07:28 +0100

    Update db_register.sql

--- formularz/db_register.sql
+++ formularz/db_register.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 28 Lis 2022, 09:33
+-- Czas generowania: 28 Lis 2022, 14:06
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.0.19
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `user_name` varchar(255) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(75) NOT NULL,
   `id` int(11) NOT NULL
@@ -39,7 +39,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_name`, `email`, `password`, `id`) VALUES
-('jan', 'jan@wp.pl', '$2y$10$n.BA9bm.8VWNE75J35oG.u6lULVQNR15OSvEkemrSs0Ut7nNLh3tO', 0);
+('jan', 'jan@wp.pl', '123', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -50,6 +50,16 @@ INSERT INTO `users` (`user_name`, `email`, `password`, `id`) VALUES
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
