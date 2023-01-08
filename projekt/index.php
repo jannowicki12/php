88916a238e616fa9a933b7d10dcaf4a3da323170
jannowicki12 <95223624+jannowicki12@users.noreply.github.com>
2023-01-08 15:51:00 +0100

    asd

--- projekt/index.php
+++ projekt/index.php
@@ -24,34 +24,50 @@ $main = new MainClass();
 		<section class="sekcja">
 			<?php
             interfaceClass::interfaceNota(); //klasa interface oraz wykorzystanie z jej funkcji
-			interfaceClass::interfaceNota1("Witaj ".$_SESSION['user_name']."#".$_SESSION['id'], "Jeśli nie wiesz, o co chodzi, to chodzi o penge
-			Wsiadam do Be eM We, tu ręka myje rękę");
-            interfaceClass::interfaceNota2("Rogal ddl - OCB/NWS", "Jeśli nie wiesz, o co chodzi, to chodzi o penge
-			Wsiadam do Be eM We, tu ręka myje rękę Tu ręka myje rękę, tu ręka rękę myje
-			Tu pięści na szczękę i się odkręcisz jak Miller
-			Tu zakładasz pętle, gdy nie przepaliłeś");
+			interfaceClass::interfaceNota1("Witaj ".$_SESSION['user_name']."#".$_SESSION['id'], "lorem");
+            interfaceClass::interfaceNota2("Rogal ddl - OCB/NWS", "lorem");
             interfaceClass::interfacePanel(); //klasa interface oraz wykorzystanie z jej funkcji
 			?>
 			            <?php if (isset($_GET['error'])) { ?>
                 <p class="error"><?php echo $_GET['error']; ?></p>
             <?php } ?>
+			<?php
+			if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
+			?>
 			<div class="alert">Galeria</div>
+			<?php
+			}
+			else {
+			?>
+				<div class="alert">Publiczna Galeria</div>
+			<?php
+			}
+			?>
 				<?php
 					// require_once ('config.class.php');
 					// $main = new MainClass();
 					$connection = $main -> db_connect(); //lączenie z bazą danych
-					
 					$query = mysqli_query($connection, "SELECT * FROM `image`") or die(mysqli_error());
-                while ($fetch = mysqli_fetch_array($query)) {
-					if($_SESSION['id'] == $fetch['id']){
-                ?>
-					<div style="border:1px solid #000; height:190px; width:190px; padding:4px; float:left; margin:10px;">
-						<a href="<?php echo $fetch['location'] ?>"><img src="<?php echo $fetch['location'] ?>" width="180" height="180"/></a>
-					</div>
-				<?php
-	            }
-			}
-			interfaceClass::interfaceNota3();
+					if (isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true) {
+						while ($fetch = mysqli_fetch_array($query)) {
+							if ($_SESSION['id'] == $fetch['id']) {
+								?>
+								<div style="border:1px solid #000; height:190px; width:190px; padding:4px; float:left; margin:10px;">
+									<a href="<?php echo $fetch['location'] ?>"><img src="<?php echo $fetch['location'] ?>" width="180" height="180"/></a>
+								</div>
+							<?php
+							}
+						}
+					}
+					else {
+					while ($fetch = mysqli_fetch_array($query)) {
+						?>
+								<div style="border:1px solid #000; height:190px; width:190px; padding:4px; float:left; margin:10px;">
+									<a href="<?php echo $fetch['location'] ?>"><img src="<?php echo $fetch['location'] ?>" width="180" height="180"/></a>
+								</div>
+						<?php
+						}
+					}
 				?>
 			</div>
 		</section>
